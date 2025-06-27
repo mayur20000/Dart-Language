21dee7cbb20c6f55ca6233bf4da2f5774f864f3e
mayur20000 <mayurvish2003@gmail.com>
2025-06-27 10:52:52 +0530

    day 1

--- lib/Built-in Data Types/Numbers/double.dart
+++ lib/Built-in Data Types/Numbers/double.dart
@@ -6,5 +6,6 @@ Example: 3.14, 9.8, 0.0
 void main() {
   double pi = 3.14159;
   double gravitationalForce = 9.81;
-  print('PI: $pi, Gravitational Force: $gravitationalForce');
+  print('PI : $pi, Gravitational Force: $gravitationalForce');
+
 }
