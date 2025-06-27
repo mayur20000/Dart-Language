265c042c37b746c788def361ad8c11692cc1369a
mayur20000 <mayurvish2003@gmail.com>
2025-06-27 11:30:51 +0530

    day1

---
+++ lib/Basic_arithmetic_operations/operations.dart
@@ -0,0 +1,27 @@
+void main() {
+  int a = 10;
+  int b = 3;
+
+  print('a + b = ${a + b}');  // 13
+  print('a - b = ${a - b}');  // 7
+  print('a * b = ${a * b}');  // 30
+  print('a / b = ${a / b}');  // 3.333... (double)
+  print('a % b = ${a % b}');  // 1 (remainder of 10/3)
+  print('a ~/ b = ${a ~/ b}'); // 3 (integer division)
+}
+
+/*
+Basic arithmetic operations:
+
++ (addition)
+
+- (subtraction)
+
+* (multiplication)
+
+/ (division, results in double)
+
+% (modulo, remainder of division)
+
+~/ (integer division, results in int)
+ */
